c569c312fb65e0b3a2e963a33df36f19d372892a
Tanapoowpat <tanapoowapat@gmail.com>
2023-10-25 20:19:31 +0700

    Update from

--- app/app.py
+++ app/app.py
@@ -11,8 +11,6 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASS')
 DB_DATABASE = os.getenv('DB_NAME')
 
-print(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE)
-
 app = Flask(__name__)
 
 # Define MySQL connection parameters
@@ -31,105 +29,120 @@ class User:
     def __init__(self, user_id, name):
         self.user_id = user_id
         self.name = name
-        self.posts = []
-        self.friends = set()  # Store friends as a set of user IDs
     
     @staticmethod
     def get_user(user_id):
         # Get user from the database
-        cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+        cursor.execute("SELECT * FROM user WHERE userID = %s", (user_id,))
         user_data = cursor.fetchone()
         if user_data:
             return User(user_data[0], user_data[1])  # Create a User object
         return None
+
+    def create_post(self, content, location_id, tagged_friends):
+        # Create a new post
+
+        location = Location.get_location(location_id)
+        post = Post(content, location, self)
         
+        # Save tagged friends to the database
+        for friend_id in tagged_friends:
+            cursor.execute("INSERT INTO posttaggedfriends (postID, userID) VALUES (%s, %s)", (post.postID, friend_id))
 
-    def addFriend(self, friend):
-        self.friends.add(friend.user_id)
-
-    def createPost(self, content, location, tagged_friends):
-        post = Post(content, location, self, tagged_friends)
+        db_connection.commit()
         return post
+    
+
+
+    @staticmethod
+    def viewpost():
+        cursor.execute("SELECT * FROM post ORDER BY timestamp DESC")
+        posts = cursor.fetchall()
+        return jsonify({"posts": posts})
+    
+    @staticmethod
+    def views_post_user_id(user):
+        cursor.execute("SELECT * FROM post WHERE user_id = %s", (user.user_id,))
+        user_posts = cursor.fetchall()
+        return jsonify({"user_posts": user_posts})
+    
+    @staticmethod
+    def viewPostsByLocation(location):
+        # Get Location from get location function
+            cursor.execute("SELECT * FROM post WHERE locationID = %s", (location.locationID,))
+            location_posts = cursor.fetchall()
+            return jsonify({"location_posts": location_posts})
+
 
 class Post:
     post_id_counter = 1
 
-    def __init__(self, content, location, user, tagged_friends):
+    def __init__(self, content, location, user):
         self.postID = Post.post_id_counter
         Post.post_id_counter += 1
         self.content = content
         self.timestamp = datetime.now()
         self.location = location
         self.user = user
-        self.taggedFriends = tagged_friends  # List of user IDs
 
         # Save the post to the database
-        cursor.execute("INSERT INTO posts (content, timestamp, location_id, user_id) VALUES (%s, %s, %s, %s)",
+        cursor.execute("INSERT INTO post (content, timestamp, locationID, user_id) VALUES (%s, %s, %s, %s)",
                        (self.content, self.timestamp, self.location.locationID, self.user.user_id))
         db_connection.commit()
+
+        
+
 
 class Location:
     def __init__(self, location_id, name):
         self.locationID = location_id
         self.name = name
         
-
     @staticmethod
     def get_location(location_id):
-        cursor.execute("SELECT * FROM locations WHERE location_id = %s", (location_id,))
+        cursor.execute("SELECT * FROM location WHERE locationID = %s", (location_id,))
         location = cursor.fetchone()
         if location:
             return Location(location[0], location[1])
         else:
             return None
         
-# REST API route to create a new post with tagged friends
-@app.route('/post', methods=['POST'])
+@app.route('/post', methods=['POST']    )
 def create_post():
-    data = request.get_json()
-    user_id = data['user_id']
-    content = data['content']
-    location_id = data['location_id']
-    tagged_friends = data.get('tagged_friends', [])
-
-    # Retrieve user and location information from the database
-    user = User.get_user(user_id)
-    location = Location.get_location(location_id)
-
-    if user and location:
-        tagged_friends = [User.get_user(friend_id) for friend_id in tagged_friends]
-        post = user.createPost(content, location, tagged_friends)
-        return jsonify({"message": "Post created successfully", "postID": post.postID})
+    data = request.json
+    user = User.get_user(data['user_id'])
+    if user:
+        user.create_post(data['content'], data['location_id'], data['tagged_friends'])
+        return jsonify({"message": "Post created successfully"})
     else:
-        return jsonify({"error": "User or location not found."})
+        return jsonify({"error": "User not found."})
 
 # REST API route to view all posts by the most recent time
 @app.route('/view', methods=['GET'])
 def view_all_posts():
-    cursor.execute("SELECT * FROM posts ORDER BY timestamp DESC")
-    posts = cursor.fetchall()
-    return jsonify({"posts": posts})
+    post = User.viewpost()
+    return post
 
 
 # REST API route to view all posts by a specific user
 @app.route('/view/user/<int:user_id>', methods=['GET'])
 def view_posts_by_user(user_id):
-    cursor.execute("SELECT * FROM posts WHERE user_id = %s", (user_id,))
-    user_posts = cursor.fetchall()
-    return jsonify({"user_posts": user_posts})
+    user = User.get_user(user_id)
+    if user:
+        post = user.views_post_user_id(user)
+        return post
+    else:
+        return jsonify({"error": "User not found."})
+    
 
 # REST API route to view all posts from a specific location
 @app.route('/view/location/<int:location_id>', methods=['GET'])
 def view_posts_by_location(location_id):
-    # Get Location from get location function
     location = Location.get_location(location_id)
     if location:
-    # GET post from database1
-        cursor.execute("SELECT * FROM posts WHERE location_id = %s", (location_id,))
-        location_posts = cursor.fetchall()
-        return jsonify({"location_posts": location_posts})
+        post = User.viewPostsByLocation(location)
+        return post
     else:
         return jsonify({"error": "Location not found."})
-
 if __name__ == '__main__':
     app.run(debug=True)
